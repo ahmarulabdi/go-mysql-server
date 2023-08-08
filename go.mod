4bf79f835d6db34c67fd13512ad622d478843045
Ahmarul Abdi <rndmjck@gmail.com>
2023-08-08 08:53:43 +0700

    add run mysql server credential setup for windows amd 64.

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module ahmarulabdi/gomysqlserver/m
 
 go 1.20
+
+require golang.org/x/term v0.11.0
+
+require golang.org/x/sys v0.11.0 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"syscall"
+
+	"golang.org/x/term"
 )
 
 const (
@@ -18,6 +21,8 @@ func main() {
 
 	if isDataDirExists(mysqlOsArch) {
 		runService(mysqlOsArch)
+	} else {
+		runSetup(mysqlOsArch)
 	}
 }
 
@@ -29,6 +34,74 @@ func runService(mysqlOsArch string) {
 		if err != nil {
 			panic(err)
 		}
+	}
+}
+
+func runSetup(mysqlOsArch string) {
+	switch mysqlOsArch {
+	case MYSQL_WINDOWS_AMD64:
+		fmt.Print("Database credential doesn't exists, please create your new credential\n\n")
+		fmt.Print("\nPassword:")
+		password := getPasswordInput()
+
+		fmt.Print("\nPassword confirmation:")
+		passwordConfirmation := getPasswordInput()
+
+		if password != passwordConfirmation {
+			fmt.Println("\n\nPassword confirmation is not same!")
+			return
+		}
+
+		fmt.Println("\nPlease wait...")
+		// init
+		cmd := exec.Command(MYSQL_WINDOWS_AMD64+"/bin/mysqld", "--initialize-insecure")
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		// run mysql service
+		runService(MYSQL_WINDOWS_AMD64)
+		fmt.Println("Run database server...")
+
+		// Command and its arguments as separate elements
+		args := []string{"-u", "root"}
+		// Create the exec.Cmd object
+		cmd3 := exec.Command(MYSQL_WINDOWS_AMD64+"/bin/mysql.exe", args...)
+		fmt.Println("Creating new credential on it...")
+
+		// Get a writable pipe to the command's standard input
+		stdin, err := cmd3.StdinPipe()
+		if err != nil {
+			fmt.Println("Error creating stdin pipe:", err.Error())
+			return
+		}
+
+		// Start the command
+		err = cmd3.Start()
+		if err != nil {
+			fmt.Println("Error starting command:", err.Error())
+			return
+		}
+
+		// set mysql root password
+		_, err = stdin.Write([]byte("ALTER USER 'root'@'localhost' IDENTIFIED BY '" + string(password) + "';\n"))
+		if err != nil {
+			fmt.Println("Error writing to command:", err.Error())
+			return
+		}
+
+		// Close the stdin pipe to indicate that we are done writing
+		stdin.Close()
+
+		// Wait for the command to finish
+		err = cmd3.Wait()
+		if err != nil {
+			fmt.Println("Error executing command:", err.Error())
+			return
+		}
+
+		fmt.Println("DONE!")
 	}
 }
 
@@ -50,4 +123,13 @@ func isDataDirExists(osArch string) bool {
 	}
 
 	return false
+}
+
+func getPasswordInput() string {
+	result, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		fmt.Println("Error reading password:", err.Error())
+	}
+
+	return string(result)
 }
